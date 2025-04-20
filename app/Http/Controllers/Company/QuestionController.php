3c9cbb26b9a12b986dddfe3deda35e7462099541
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-04-20 13:53:48 +0600

    fixing

--- app/Http/Controllers/Company/QuestionController.php
+++ app/Http/Controllers/Company/QuestionController.php
@@ -280,7 +280,7 @@ class QuestionController extends Controller
             foreach ($question->answer as $ans) {
             $data[] = [
                 'emoji_or_star' => $survey->emoji_or_star,
-                'participant' => $survey->user->anonymous === true ? 'abc' : 'no name',
+                'participant' => $survey->user->anonymous === true ? 'Anonymous' : $ans->user->email,
                 'user_id' => $ans->user_id,
                 'project_id' => $survey->project_id,
                 'project_name' => $survey->project->project_name,
